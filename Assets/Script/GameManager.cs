8affa0cd55e2e909266f8fbf0e33acb1254dba4f
CliffLee <tommyboys0107@gmail.com>
2015-09-06 01:18:06 +0800

    Retrieve ScoreManager, GameManager script

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager instance;
+	public GameState gamesState;
+
+	public enum GameState{
+		MainMenu,
+		InLevel,
+		GameSucceed,
+		GameOver
+	};
+	
+	void Awake () {
+		if(instance == null){
+			instance = this;
+		}
+		else if(instance != this){
+			Destroy(this);
+		}
+
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void OnEnable(){
+		EventManager.OnReturnMenu += ReturnMenu;
+		EventManager.OnGameStart += GameStart;
+		EventManager.OnGameSucceed += GameSucceed;
+		EventManager.OnGameOver += GameOver;
+	}
+
+	void OnDisable(){
+		EventManager.OnReturnMenu -= ReturnMenu;
+		EventManager.OnGameStart -= GameStart;
+		EventManager.OnGameSucceed -= GameSucceed;
+		EventManager.OnGameOver -= GameOver;
+	}	
+
+	void ReturnMenu(){
+		gamesState = GameState.MainMenu;
+	}
+
+	void GameStart(){
+		gamesState = GameState.InLevel;
+	}
+
+	void GameSucceed(){
+		gamesState = GameState.GameSucceed;
+	}
+
+	void GameOver(){
+		gamesState = GameState.GameOver;
+	}
+}
+
