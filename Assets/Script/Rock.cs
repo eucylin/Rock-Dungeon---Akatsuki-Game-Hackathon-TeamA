bf216f4680ffaa370d80a9e450ab7bf3a90dac75
CliffLee <tommyboys0107@gmail.com>
2015-09-06 03:30:33 +0800

    Rock script half-way

---
+++ Assets/Script/Rock.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rock : MonoBehaviour {
+	public bool isPushing = false, isPulling = false;
+
+	// Update is called once per frame
+	void Update () {
+		if(isPushing){
+			PushMove();
+		}
+		else if(isPulling){
+			PullMove();
+		}
+	}
+
+	public void Push(float playerX, float playerZ){
+		int pX = Mathf.FloorToInt(playerX), pZ = Mathf.FloorToInt(playerZ),
+		rX = Mathf.FloorToInt(transform.position.x), rZ = Mathf.FloorToInt(transform.position.z);
+
+		isPushing = true;
+	}
+
+	public void Pull(){
+		isPulling = true;
+	}
+
+	void PushMove(){
+
+	
+	}
+
+	void PullMove(){
+	
+	}
+}
