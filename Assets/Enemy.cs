91e11d09e8f3a8cd0e147394d593c033c0e89310
zxc10806 <zxc10806@yahoo.com.tw>
2015-09-05 21:15:46 +0800

    enemy Update

---
+++ Assets/Enemy.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	public GameObject player; 
+	public float speed;
+	public float enemyHealth;
+	public bool playerInSight;
+	public Vector3 targetPosition;
+	public bool[,] visitedGrid;
+	public int [,] direction;
+	public int [,] searchDirection;
+
+	// Use this for initialization
+	void Start () {
+		speed = 1.0f;
+		enemyHealth = 10.0f;
+		playerInSight = false;
+		direction = new int[4, 2];
+
+		direction [0,0] = -1;
+		direction [0,1] = 0;
+
+		direction [1,0] = 0;
+		direction [1,1] = 1;
+
+		direction [2,0] = 1;
+		direction [2,1] = 0;
+
+		direction [3,0] = 0;
+		direction [3,1] = -1;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if enemy dies destroy
+		if (enemyHealth <= 0.0f)
+			Destroy (gameObject);
+
+		//
+		if (playerInSight == true)
+			Attack ();
+		else
+			Patrol ();
+
+	}
+
+
+	//Attack
+	void Attack()
+	{
+		//chase player
+
+	}
+	//巡邏
+	void Patrol()
+	{
+		//walk to target position
+
+	}
+
+	void WalkGrid()
+	{
+		Quaternion dir = gameObject.transform.rotation;
+
+	}
+
+	void OnTriggerStay (Collider other) {
+		if (other.gameObject == player)
+		{
+			playerInSight = false;
+		}
+			
+	}
+
+}
---
+++ Assets/EnemyManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+public class EnemyManager : MonoBehaviour
+{
+
+	public GameObject enemy;                // The enemy prefab to be spawned.
+	public float spawnTime = 3.0f;            // How long between each spawn.
+	//public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+	public bool[] blocked;
+	public Vector3[] spawnPoints;  
+	public float currentTime;
+
+
+	void Start ()
+	{
+		currentTime = 2.0f;
+		for (int i = 0; i<blocked.Length; i++)
+			blocked [i] = false;
+		Spawn();
+		// Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+		//InvokeRepeating ("Spawn", spawnTime, spawnTime);
+	}
+	
+	void Update()
+	{
+		currentTime += Time.deltaTime;
+		if (currentTime >= spawnTime) {
+			currentTime = 0.0f;
+			//Spawn();
+
+		}
+	}
+	void Spawn ()
+	{
+		// Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+		Instantiate (enemy, new Vector3(0,2,0), Quaternion.identity);
+	}
+}
