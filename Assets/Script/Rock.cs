9d748a8e9717aef36533ba9014f4503b8e950477
CliffLee <tommyboys0107@gmail.com>
2015-09-06 04:32:49 +0800

    Push is part-done, need to test.
    Add Rock script
    Add "Rock" tag
    Add Rock prefab
    Add CliffTestScene scene for my testing

--- Assets/Script/Rock.cs
+++ Assets/Script/Rock.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Rock : MonoBehaviour {
 	public bool isPushing = false, isPulling = false;
+	int dirX, dirZ, targetX, targetZ, forceCoef;
 
 	// Update is called once per frame
 	void Update () {
@@ -14,11 +15,18 @@ public class Rock : MonoBehaviour {
 		}
 	}
 
-	public void Push(float playerX, float playerZ){
+	public void Push(float playerX, float playerZ, int force = 1){
 		int pX = Mathf.FloorToInt(playerX), pZ = Mathf.FloorToInt(playerZ),
 		rX = Mathf.FloorToInt(transform.position.x), rZ = Mathf.FloorToInt(transform.position.z);
 
-		isPushing = true;
+		dirX = rX - pX;
+		dirZ = rZ - pZ;
+		if(dirX != 0 ^ dirZ != 0){
+			targetX = dirX * force + rX;
+			targetZ = dirZ * force + rZ;
+			forceCoef = force;
+			isPushing = true;
+		}
 	}
 
 	public void Pull(){
@@ -26,8 +34,12 @@ public class Rock : MonoBehaviour {
 	}
 
 	void PushMove(){
-
-	
+		if(Mathf.FloorToInt(transform.position.x) != targetX){
+			transform.position = new Vector3(Mathf.MoveTowards(transform.position.x, targetX, Time.deltaTime * forceCoef), transform.position.y, transform.position.z);
+		}
+		else if(Mathf.FloorToInt(transform.position.z) != targetZ){
+			transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.MoveTowards(transform.position.z, targetZ, Time.deltaTime * forceCoef));
+		}
 	}
 
 	void PullMove(){
